b3ca9e65f9a65be5461e2b67ed0af95f88659fa6
Francisco Casillas <chiccasillas@gmail.com>
2025-01-23 11:03:02 -0600

    Create a basic card using Text, auxiliary views, modifiers. Stage, commit and push the project to Github.

--- BusinessCardApp/ContentView.swift
+++ BusinessCardApp/ContentView.swift
@@ -10,13 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tarjeta de presentación")
+                .font(.title)
+                .underline()
+            VStack {
+               NameView()
+               DescriptionView()
+            }
         }
         .padding()
     }
+}
+
+struct NameView: View {
+    var body: some View {
+        VStack{
+            Text("Francisco Casillas")
+                .font(.custom("Jersey15-Regular", size: 22))
+                .foregroundColor(Color("Green"))
+            Text("iOS Developer")
+                .bold()
+        }
+        .padding()
+    }
+}
+
+struct DescriptionView: View {
+    var body: some View{
+        Text("Me gusta las oreos y comprar productos Mac en los centros comerciales después de las tres de la tarde.")
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+       
 }
 
 #Preview {
