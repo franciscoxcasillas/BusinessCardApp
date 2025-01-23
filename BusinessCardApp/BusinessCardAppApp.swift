26a325a0f07276b7188f5460f6e69983f5cbfcc2
Francisco Casillas <chiccasillas@gmail.com>
2025-01-23 09:43:20 -0600

    Initial Commit

---
+++ BusinessCardApp/BusinessCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardAppApp.swift
+//  BusinessCardApp
+//
+//  Created by Francisco Jean on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
